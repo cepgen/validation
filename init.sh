c21ada8ba64cb232140cd580a832411c68e8a467
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-02-13 17:22:25 +0100

    Added initialisation script

---
+++ init.sh
@@ -0,0 +1,26 @@
+if [ -z $CEPGEN_PATH ]; then
+  echo "Variable \$CEPGEN_PATH is unset."
+  return
+else
+  echo "Variable \$CEPGEN_PATH is set to '$CEPGEN_PATH'."
+fi
+
+cat << EOF > CepGenEnvironment.h
+#ifndef CepGenEnvironment_h
+#define CepGenEnvironment_h
+
+// clang-format off
+R__ADD_INCLUDE_PATH($CEPGEN_PATH)
+R__ADD_LIBRARY_PATH($CEPGEN_PATH/build)
+R__LOAD_LIBRARY(libCepGen.so)
+// clang-format on
+
+#endif
+EOF
+
+if [ ! -f a08tmc.dat ]; then
+  ln -s $CEPGEN_PATH/External/a08tmc.dat a08tmc.dat
+fi
+if [ ! -f mstw_sf_scan_nnlo.dat ]; then
+  ln -s $CEPGEN_PATH/External/mstw_sf_scan_nnlo.dat mstw_sf_scan_nnlo.dat
+fi
